b4b3ff782c896bf1a4a0deddc7c59a607a390240
Camila Nepomuceno <camila.cno@gmail.com>
2022-12-09 18:30:22 -0300

    Add fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { ActivityIndicator } from 'react-native'
 import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 import defaultTheme from '@theme/default'
 import { Groups } from '@screens/Groups'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   )
 }
