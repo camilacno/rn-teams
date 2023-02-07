25fb58b0776168005b7ad4988a660ae8c98e429b
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-07 14:56:53 -0300

    Handling exceptions

--- src/screens/NewGroup.tsx
+++ src/screens/NewGroup.tsx
@@ -1,19 +1,34 @@
 import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { Alert } from 'react-native'
 
 import { Button, Header, Highlight } from '@components/index'
 import { Container, Content, UsersIcon } from './NewGroupStyles'
 import { Input } from '@components/Input'
 
 import { groupCreate } from '@storage/group/groupCreate'
+import { AppError } from '@utils/AppError'
 
 export function NewGroup() {
   const [group, setGroup] = useState('')
   const navigation = useNavigation()
 
   async function handleCreateNewGroup() {
-    await groupCreate(group)
-    navigation.navigate('players', { group })
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo grupo', 'Informe o nome do grupo')
+      }
+
+      await groupCreate(group)
+      navigation.navigate('players', { group })
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Novo grupo', error.message)
+      } else {
+        Alert.alert('Novo grupo', 'Não foi possível criar o novo grupo.')
+        console.log(error)
+      }
+    }
   }
 
   return (
