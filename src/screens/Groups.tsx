4da4aba7a211f8283c397f5c89f29831a683d77f
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-07 15:39:42 -0300

    Create new group

--- src/screens/Groups.tsx
+++ src/screens/Groups.tsx
@@ -17,6 +17,14 @@ export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Turma 1'])
   const navigation = useNavigation()
 
+  function handleNewGroup() {
+    navigation.navigate('newGroup')
+  }
+
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group })
+  }
+
   async function fetchGroups() {
     try {
       const data = await groupsGetAll()
@@ -32,10 +40,6 @@ export function Groups() {
     }, [])
   )
 
-  function handleNavigateToNewGroup() {
-    navigation.navigate('newGroup')
-  }
-
   return (
     <Container>
       <Header />
@@ -45,13 +49,19 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => <GroupCard key={item} title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard
+            key={item}
+            title={item}
+            onPress={() => handleOpenGroup(item)}
+          />
+        )}
         ListEmptyComponent={() => (
           <EmptyList message="Sem turmas cadastrados. Cadastre uma nova turma!" />
         )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
       />
-      <Button title="Criar nova turma" onPress={handleNavigateToNewGroup} />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   )
 }
